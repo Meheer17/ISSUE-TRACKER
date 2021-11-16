c884aa10d85fd74186a25eaa535e04e679e66d79
Meheer007 <>
2021-11-16 10:37:01 +0000

    last

--- models.js
+++ models.js
@@ -4,13 +4,12 @@ const {Schema} = mongoose;
 const IssueSchema = new Schema({
     issue_title: {type: String, required: true},
     issue_text: {type: String, required:true},
-    created_on: Date,
-    updated_on: Date,
     created_by: {type: String, required:true},
     assigned_to: String,
     open: Boolean,
     status_text: String
-});
+} ,
+ { timestamps: { createdAt: "created_on", updatedAt: "updated_on" }} );
 const Issue = mongoose.model("Issue", IssueSchema);
 
 const ProjectSchema = new Schema({
@@ -20,4 +19,4 @@ const ProjectSchema = new Schema({
 const Project = mongoose.model("Project", ProjectSchema);
 
 exports.Issue = Issue;
-exports.Project = Project;  
+exports.Project = Project;
--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,7 @@ const IssueModel = require("../models").Issue;
 const ProjectModel = require("../models").Project;
 const ObjectId = mongoose.Types.ObjectId;
 
+
 module.exports = function (app) {
 
   app
@@ -58,15 +59,15 @@ module.exports = function (app) {
       const newIssue = new IssueModel({
           issue_title: issue_title || "" ,
           issue_text: issue_text || "" ,
-          created_on: new Date() ,
-          updated_on: new Date() ,
           created_by:  created_by || "" ,
           assigned_to: assigned_to || "" ,
           open: true,
+          created_on: new Date() ,
+          updated_on: new Date() ,
           status_text:  status_text || "",
       });
       ProjectModel.findOne({name: project}, (err, prdata) => {
-          if(!prdata || err){
+          if(!prdata){
               const newProject = new ProjectModel({name: project});
               newProject.issues.push(newIssue);
               newProject.save((err, data) => {
@@ -94,14 +95,20 @@ module.exports = function (app) {
     .put(function (req, res){
       let project = req.params.project;
       const {
-          _id, issue_title, issue_text, created_by, assigned_to, status_text, open
+          _id, 
+          issue_title, 
+          issue_text, 
+          created_by, 
+          assigned_to, 
+          status_text,
+          open,
       } = req.body;
       if(!_id){
           res.json({error: "missing _id"});
           return;
       }
       if(
-          !issue_text && !issue_text && !created_by && !assigned_to && !status_text && !open
+          !issue_title && !issue_text && !created_by && !assigned_to && !status_text && !open
       ) {
           res.json({ error: 'no update field(s) sent', '_id': _id });
           return;
@@ -124,11 +131,11 @@ module.exports = function (app) {
             issueData.created_by = created_by || issueData.created_by;
             issueData.assigned_to = assigned_to || issueData.assigned_to;
             issueData.status_text = status_text || issueData.status_text;
-            issueData.updated_on = new Date();
             issueData.open = open;
             projectdata.save((err, data) => {
                 if(err || !data) {
                     res.json({error : "could not update", "_id": _id});
+                    console.lod('ohno')
                 } else {
                     res.json({  result: 'successfully updated', '_id': _id });
                 }
@@ -141,31 +148,35 @@ module.exports = function (app) {
     .delete(function (req, res){
       let project = req.params.project;
       const {_id} = req.body;
+
       if(!_id){
-          res.json({ error: 'missing _id' });
-          return;
-      } 
-      ProjectModel.findOne  ({name: project}, (err, projectdata) => {
-          if(!projectdata || err){
-            res.json({error: "could not delete", _id: _id})
+        res.json({ error: 'missing _id' });
+        return;
+        }
+      
+      
+      ProjectModel.findOne({name: project}, (err, projectdata) => {
+          if(!projectdata){
+            res.json({error: "could not delete", "_id": _id})
           } else {
               const issueData = projectdata.issues.id(_id);
               if(!issueData){
-                  res.json({error: "could not delete", _id: _id});
+                  res.send({error: "could not delete", "_id": _id});
                   return;
               }
               issueData.remove();
 
               projectdata.save((err, data) => {
-                  if(err || !data){
+                  if(!data){
                       res.json({ error: 'could not delete', '_id': _id });
                   } else {
-                      res.json({ result: 'successfully deleted', '_id': _id })
+                      res.json({ result: 'successfully deleted', "_id": _id })
                   }
               })
           }
       });
       
+
     });
     
 };
